1097dd8799041b03b4ec845475ddac569e3f1b9c
Saim Afzal <saimafzal142@gmail.com>
2025-09-25 23:56:01 +0300

    set the position of dashboards cards

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,21 @@
 import { AppLineChart } from "@/components/AppLineChart";
 import DashboardCards from "@/components/DashboardCards";
 import { TopPerformingLocations } from "@/components/TopPerformingLocations";
+// import { DataTable } from "@/components/DataTables";
 
 export default function Home() {
   return (
    <main className="px-8">
    <div className="grid gap-4 col-span-1 lg:col-span-2 2xl:col-span-4">
     {/* cards */}
-   <div><DashboardCards /></div>
+   <div className="mt-6 p-6"><DashboardCards /></div>
    {/* linechart */}
    <div><AppLineChart /></div>
    {/* tables */}
-   <div>test</div>
+   <div>
+    {/* <DataTable /> */}
+    test
+   </div>
    {/* locations */}
    <div><TopPerformingLocations /></div>
    </div>
--- src/components/DashboardCards.tsx
+++ src/components/DashboardCards.tsx
@@ -1,43 +1,68 @@
 import React from "react"
 import { AddUser } from "./AddUser"
+import { Card, CardContent, CardHeader, CardTitle } from "./ui/card"
 
 function DashboardCards() {
   return (
     <main className="">
       {/* Header row */}
       <div className="flex items-center justify-between">
-        <h1 className="text-2xl font-bold">Dashboard</h1>
-        <div className="flex items-center rounded-md bg-orange-600 text-black font-medium shadow hover:bg-orange-700 active:translate-y-[1px]">
+        <h1 className="text-2xl font-bold mb-4">Dashboard</h1>
+        <div className="flex items-center rounded-md bg-[#FF8904] text-black font-medium shadow hover:bg-orange-700 active:translate-y-[1px]">
           <span><AddUser /></span>
         </div>
       </div>
 
       {/* Cards row */}
       <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-4 gap-4">
-  {/* Card 1 */}
-  <div className="text-card-foreground flex flex-col justify-between gap-6 py-6 shadow-sm rounded-xl bg-gradient-to-t from-orange-50 to-white">
-    <p className="text-gray-600">Total Users</p>
-    <h2 className="text-2xl font-bold">4</h2>
-  </div>
+        {/* Card 1 */}
+        <Card className="shadow-sm border rounded-xl bg-gradient-to-t from-orange-50 to-white">
+          <CardHeader>
+            <CardTitle className="text-sm font-medium text-muted-foreground">
+              Total Users
+            </CardTitle>
+          </CardHeader>
+          <CardContent>
+            <p className="text-xl sm:text-2xl font-bold">4</p>
+          </CardContent>
+        </Card>
 
-  {/* Card 2 */}
-  <div className="text-card-foreground flex flex-col justify-between gap-6 py-6 shadow-sm rounded-xl bg-gradient-to-t from-orange-50 to-white">
-    <p className="text-gray-600">Total Inquiries</p>
-    <h2 className="text-2xl font-bold">104</h2>
-  </div>
+        {/* Card 2 */}
+        <Card className="shadow-sm border rounded-xl bg-gradient-to-t from-orange-50 to-white">
+          <CardHeader>
+            <CardTitle className="text-sm font-medium text-muted-foreground">
+              Total Inquiries
+            </CardTitle>
+          </CardHeader>
+          <CardContent>
+            <p className="text-xl sm:text-2xl font-bold">99</p>
+          </CardContent>
+        </Card>
 
-  {/* Card 3 */}
-  <div className="text-card-foreground flex flex-col justify-between gap-6 py-6 shadow-sm rounded-xl bg-gradient-to-t from-orange-50 to-white">
-    <p className="text-gray-600">Total Locations</p>
-    <h2 className="text-2xl font-bold">55</h2>
-  </div>
+        {/* Card 3 */}
+        <Card className="shadow-sm border rounded-xl bg-gradient-to-t from-orange-50 to-white">
+          <CardHeader>
+            <CardTitle className="text-sm font-medium text-muted-foreground">
+              Total Locations
+            </CardTitle>
+          </CardHeader>
+          <CardContent>
+            <p className="text-xl sm:text-2xl font-bold">50</p>
+          </CardContent>
+        </Card>
 
-  {/* Card 4 */}
-  <div className="text-card-foreground flex flex-col justify-between gap-6 py-6 shadow-sm rounded-xl bg-gradient-to-t from-orange-50 to-white">
-    <p className="text-gray-600">Total Reservations</p>
-    <h2 className="text-2xl font-bold">0</h2>
-  </div>
-</div>
+        {/* Card 4 */}
+        <Card className="shadow-sm border rounded-xl bg-gradient-to-t from-orange-50 to-white">
+          <CardHeader>
+            <CardTitle className="text-sm font-medium text-muted-foreground">
+              Total Reservations
+            </CardTitle>
+          </CardHeader>
+          <CardContent>
+            <p className="text-xl sm:text-2xl font-bold">3</p>
+          </CardContent>
+        </Card>
+      </div>
 
     </main>
   )
